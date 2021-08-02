32bb5063432936737e3b95e6b9b1cd206476cd2e
mayaraviviancruz <78976475+mayaraviviancruz@users.noreply.github.com>
2021-08-02 10:13:20 -0300

    Update envio.sh

--- Projeto-braco/envio.sh
+++ Projeto-braco/envio.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gnome-terminal -- /bin/bash /home/joao-ishida/projetoEmbarcados2021/prjMotorEncoder/sendReal.sh $1 $2
+gnome-terminal -- /bin/bash /home/mayara-cruz/embarcados2021/Projeto-braco/envio.sh $1 $2
